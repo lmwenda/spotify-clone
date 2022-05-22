4d1827f3b0a99fe06aa7eef273835d864ccdd757
Doflamingo <67679722+lmwenda@users.noreply.github.com>
2022-05-22 18:51:20 +0100

    production url

--- src/spotify.js
+++ src/spotify.js
@@ -4,7 +4,7 @@
 // Initializations
 
 export const authEndpoint = "https://accounts.spotify.com/authorize";
-const redirectUri = "http://localhost:3000/";
+const redirectUri = "https://spotify-clone-82yh5iq9t-lmwenda.vercel.app/";
 const clientId = "e73f5ab8c40149b4b63a202d49311222";
 const scopes = [
     "user-read-currently-playing",
